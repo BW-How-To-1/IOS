d43d308e1295f3d0f3502be9a2add38d1f18f71a
Cody Morley <codymorleyinc@gmail.com>
2020-05-26 08:49:21 -0700

    added and signed two classes for network control.

---
+++ HowTo/HowTo/LoginSignupController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginSignupController.swift
+//  HowTo
+//
+//  Created by Cody Morley on 5/26/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class LoginSignupController {
+    
+}
