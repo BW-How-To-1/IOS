66c8053c5ca97bbd8c1070dd90649a5a1b8eec6a
Cody Morley <codymorleyinc@gmail.com>
2020-05-26 09:02:06 -0700

    added bearer token poperty. if back-end uses UUID for user account authentication rather than bearer tokens, this is pretty easily re-factored.

---
+++ HowTo/HowTo/Bearer.swift
@@ -0,0 +1,15 @@
+//
+//  Bearer.swift
+//  HowTo
+//
+//  Created by Bling Morley on 5/26/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    //TODO: - Add login variables according to back-end model. Leaving token here for now since that's the most likely to remain the norm.
+    
+    var token: String
+}
--- HowTo/HowTo/LoginSignupController.swift
+++ HowTo/HowTo/LoginSignupController.swift
@@ -10,5 +10,30 @@ import Foundation
 import CoreData
 
 class LoginSignupController {
+    //MARK: - Enums & Type Aliases -
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+        case put = "PUT"
+        case delete = "DELETE"
+        case patch = "PATCH"
+        case head = "HEAD"
+        case options = "OPTIONS"
+        case connect = "CONNECT"
+    }
+    
+    
+    //MARK: - Properties -
+    var bearer: Bearer?
+    
+    
+    
+    //MARK: - Actions -
+    
+    
+    
+    
+    //MARK: - Methods -
+    
     
 }
