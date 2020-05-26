69470d2e3e438f31af38033cc2f588bb19746f35
Cody Morley <codymorleyinc@gmail.com>
2020-05-26 09:45:11 -0700

    some more work. just trying to keep good git hygiene more than anything. finished stubbing properties table for login and sign up controller.

--- HowTo/HowTo/LoginSignupController.swift
+++ HowTo/HowTo/LoginSignupController.swift
@@ -26,14 +26,21 @@ class LoginSignupController {
     //MARK: - Properties -
     var bearer: Bearer?
     
+    private var baseURL: URL(string: "www.google.com/") //TODO: Add baseURL from back-end or firebase DB here.
+    private var loginURL = baseURL.appendPathComponent("login") //TODO: Add correct path component
+    private var signupURL = baseURL.appendPathComponent("signup") //TODO: Add correct path component.
+    
+    private lazy var jsonEncoder = JSONEncoder()
+    private lazy var jsonDecoder = JSONDecoder()
     
     
     //MARK: - Actions -
+    /// login and signup actions go here
     
     
     
     
     //MARK: - Methods -
-    
+    ///URLHandlers, decoding helpers, etc. go here.
     
 }
