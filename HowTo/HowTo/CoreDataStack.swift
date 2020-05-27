2bca8967db193220cf5526d000a0260f696ba96d
Cody Morley <codymorleyinc@gmail.com>
2020-05-26 20:27:25 -0700

    added second save method to core data stack that will allow saving easily on alternate contexts. Method throws.

--- HowTo/HowTo/CoreDataStack.swift
+++ HowTo/HowTo/CoreDataStack.swift
@@ -30,4 +30,17 @@ class CoreDataStack {
         persistentContainer.viewContext
     }
     
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        var error: Error?
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
 }
