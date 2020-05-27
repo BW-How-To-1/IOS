464a38cfd148646a7434131a63c36c4e9968e8b0
Shawn James <shawn.james@me.com>
2020-05-27 17:09:02 -0400

    Add live login unit test

--- HowTo/HowToTests/HowToTests.swift
+++ HowTo/HowToTests/HowToTests.swift
@@ -10,24 +10,34 @@ import XCTest
 @testable import HowTo
 
 class HowToTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    /*
+     
+     Does decoding work?
+     Does decoding fail when given bad data?
+     Does it build the correct URL?
+     Does it build the correct URLRequest?
+     Are the search results saved properly?
+     Is the completion handler called when data is good?
+     Is the completion handler called when data is bad?
+     Is the completion handler called when networking fails?
+     
+     */
+        
+        // MARK: - Live data
+        
+        func testLiveLogin() {
+            let loginSignupController = LoginSignupController()
+            let loginExpectation = XCTestExpectation(description: "Wait for login result")
+            let testUser = User(username: "Testing", password: "Testing")
+            
+            loginSignupController.logIn(as: testUser) { (result) in
+                print("The login responded with something.")
+                XCTAssertNotNil(loginSignupController.bearer?.token)
+                loginExpectation.fulfill()
+            }
+            wait(for: [loginExpectation], timeout: 5)
+            XCTAssertNotNil(loginSignupController.bearer?.token)
         }
     }
 
