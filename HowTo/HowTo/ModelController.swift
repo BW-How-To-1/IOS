3e5b57103e1576b1be1b0418e616d21534f1c0fc
Cody Morley <codymorleyinc@gmail.com>
2020-05-28 11:25:11 -0700

    working outline of ModelController.swift

---
+++ HowTo/HowTo/ModelController.swift
@@ -0,0 +1,42 @@
+//
+//  ModelController.swift
+//  HowTo
+//
+//  Created by Cody Morley on 5/28/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ModelController {
+    //MARK: - Properties -
+    var shared = ModelController()
+    
+    
+    //MARK: - Actions -
+    ///This Controller only holds the methods for creation and deletion in Core Data. For read/synch methods, use the update functions of NetworkController.swift
+    func createTutorial() {
+        
+    }
+    
+    func createComment() {
+        
+    }
+    
+    
+    func deleteTutorial() {
+        
+    }
+    
+    func deleteComment() {
+        
+    }
+    
+    
+    
+    
+    
+    //MARK: - Methods -
+    
+}
