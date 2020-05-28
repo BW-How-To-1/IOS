74b75ed5bc30568ba29188a24ae8eaee01e944e1
Cody Morley <codymorleyinc@gmail.com>
2020-05-28 10:27:45 -0700

    gave network controller comment fetching, updating, and synching functionality. Moving on to post and delete.

--- HowTo/HowTo/NetworkController.swift
+++ HowTo/HowTo/NetworkController.swift
@@ -30,6 +30,7 @@ class NetworkController {
     
     typealias CompletionHandler = (Result<Bool, NetworkError>) -> Void
     typealias TutorialHandler = (Result<[Tutorial], NetworkError>) -> Void
+    typealias CommentHandler = (Result<[Comment], NetworkError>) -> Void
     
     //MARK: - Properties -
     lazy var bearer: Bearer? = LoginSignupController.shared.bearer
@@ -38,10 +39,12 @@ class NetworkController {
     let postURL = URL(string: "https://howto-56e14.firebaseio.com/")!
     let getURL = URL(string: "https://howto-56e14.firebaseio.com/")!
     let tutorialURL = URL(string: "https://howto-56e14.firebaseio.com/")!
+    let commentURL = URL(string: "https://howto-56e14.firebaseio.com/")!
 //    let baseURL = URL(string: "https://how-to-diy.herokuapp.com/api/")!
 //    lazy var postURL = baseURL.appendingPathComponent("/posturl/") //TODO: NEEDS CORRECT ENDPOINT
 //    lazy var getURL = baseURL.appendingPathComponent("/geturl") //TODO: NEEDS CORRECT ENDPOINT
 //    lazy var tutorialURL = baseURL.appendingPathComponent("/tutorialID/") //TODO: NEEDS CORRECT ENDPOINT
+//    lazy var commentURL = baseURL.appendingPathComponent("/tutorialID/commentID") //TODO: NEEDS CORRECT ENDPOINT
     
     var jsonEncoder = JSONEncoder()
     var jsonDecoder = JSONDecoder()
@@ -131,9 +134,6 @@ class NetworkController {
                 return
             }
         }.resume()
-        
-        
-        
     }
     
     ///update local how-tos with server
@@ -194,9 +194,84 @@ class NetworkController {
         }.resume()
     }
     
+    func getComments(completion: @escaping CommentHandler) {
+        let request = getRequest(for: commentURL)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                NSLog("Error when retrieving comments from server: \(error) \(error.localizedDescription)")
+                completion(.failure(.otherError))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200 else {
+                    NSLog("Error: Bad or no response from server when retrieving comments from server.")
+                    completion(.failure(.badResponse))
+                    return
+            }
+            
+            guard let data = data else {
+                NSLog("Error: No data recieved when retrieving comments from server.")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let allCommentReps = try self.jsonDecoder.decode([CommentRepresentation].self, from: data)
+                var allComments:[Comment] = []
+                for rep in allCommentReps {
+                    if let newComment = Comment(representation: rep) {
+                        allComments.append(newComment)
+                    }
+                }
+                try self.updateComments(with: allCommentReps)
+                completion(.success(allComments))
+            } catch {
+                NSLog("Error decoding comment data: \(error) \(error.localizedDescription)")
+                completion(.failure(.noDecode))
+                return
+            }
+        }.resume()
+    }
+    
+    func updateComments(with representations: [CommentRepresentation]) throws {
+        let identifiersToFetch = representations.compactMap { $0.id }
+        let representationsByID = Dictionary(uniqueKeysWithValues: zip(identifiersToFetch, representations))
+        var commentsToCreate = representationsByID
+        
+        let fetchRequest: NSFetchRequest<Comment> = Comment.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id IN %@", identifiersToFetch)
+        let context = CoreDataStack.shared.mainContext
+        
+        do {
+            let existingComments = try context.fetch(fetchRequest)
+            for comment in existingComments {
+                let idString = comment.id!.uuidString
+                guard let representation = representationsByID[idString] else { continue }
+                self.editComment(comment: comment, with: representation)
+                commentsToCreate.removeValue(forKey: idString)
+            }
+            for representation in commentsToCreate.values {
+                Comment(representation: representation)
+            }
+        } catch {
+            NSLog("Error: Failed updating comments")
+        }
+        try CoreDataStack.shared.mainContext.save()
+    }
+    
+    func postComment() {
+    
+    }
+    
+    func deleteComment() {
+        
+    }
+    
     
     //MARK: - Methods -
-    ///update helper
+    ///update helpers
     private func update(tutorial: Tutorial, with representation: TutorialRepresentation) {
         tutorial.author = representation.author
         tutorial.bodyText = representation.bodyText
@@ -205,6 +280,11 @@ class NetworkController {
         tutorial.image = representation.image
         tutorial.likes = representation.likes
         tutorial.title = representation.title
+    }
+    
+    private func editComment(comment: Comment, with representation: CommentRepresentation) {
+        comment.title = representation.title
+        comment.text = representation.text
     }
     
     ///create methods helpers
